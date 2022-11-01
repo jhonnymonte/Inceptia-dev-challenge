a25439c10f6485530c5035e576561067dab0985e
jonatan_montenegro <mrmontenegro@gmail.com>
2022-11-01 18:55:23 -0300

    unittest fix

--- ejercicio_2.py
+++ ejercicio_2.py
@@ -4,9 +4,9 @@ from loguru import logger
 _PRODUCT_DF = pd.DataFrame({"product_name": ["Chocolate","Granizado", "Limon", "Dulce de Leche"], "quantity":[3,10,0,5]})
 
 def is_product_available(product_name, quantity):
-
+    list_of_products = _PRODUCT_DF
     try:
-        _PRODUCT_DF.set_index("product_name",inplace=True)
+        list_of_products.set_index("product_name",inplace=True)
         value = _PRODUCT_DF.loc[product_name]
         if quantity <= value["quantity"]:
             return  True
@@ -15,3 +15,5 @@ def is_product_available(product_name, quantity):
     except Exception as ex:
         logger.info(f'el producto {product_name} no es valido', {ex})
         
+
+
--- ejercicio_3.py
+++ ejercicio_3.py
@@ -1,12 +1,4 @@
 from loguru import logger
-
-"""
-Ejercicio 3:
-Completar la función validate_discount_code con el siguiente objetivo:
-● Dada la lista de códigos de descuento vigentes y un código de descuento mencionado por el cliente, devuelve True si la diferencia entre el código mencionado y los códigos vigentes es menor a tres caracteres, en al menos uno de los casos.
-Por diferencia se entiende: caracteres que están presentes en el código brindado, pero no en el código evaluado de la lista o viceversa.
-"""
-
 _AVAILABLE_DISCOUNT_CODES = ["Primavera2021", "Verano2021", "Navidad2x1",
 "heladoFrozen"]
 
@@ -30,6 +22,6 @@ def validate_discount_code(discount_code):
     except Exception as ex:
         logger.info(ex)
 
-print(validate_discount_code("primavera2021"))
+
 
 
